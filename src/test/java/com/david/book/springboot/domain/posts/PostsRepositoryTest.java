d9c6e9c5bca9f5ec0dcd667406442794de0fc810
david <abovenormal5023@gmail.com>
2022-07-05 12:55:08 +0900

    ~p107 service, repository

---
+++ src/test/java/com/david/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.david.book.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After          // 여러 테스트가 동시에 수행되면 테스트용 데이터베이스 h2에 데이터가 그대로 남아 있어 다음 테스트 실행 시 실패할 수 있기 때문.
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글_불러오기(){
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title).content(content).author("abovenormal5023@gmail.com").build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
