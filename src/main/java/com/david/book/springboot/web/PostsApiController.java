45c178871cc3c228f846af954c73df1eca894093
david <abovenormal5023@gmail.com>
2022-07-15 00:12:54 +0900

    ~p115 apicontrollertest 완료 ~p116 h2 db check 완료

--- src/main/java/com/david/book/springboot/web/PostsApiController.java
+++ src/main/java/com/david/book/springboot/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.david.book.springboot.web;
 
 import com.david.book.springboot.service.posts.PostsService;
+import com.david.book.springboot.web.dto.PostsResponseDto;
 import com.david.book.springboot.web.dto.PostsSaveRequestDto;
+import com.david.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,6 +18,14 @@ public class PostsApiController {
         return postsService.save(requestDto);
     }
 
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id,requestDto);
+    }
 
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
 
 }
