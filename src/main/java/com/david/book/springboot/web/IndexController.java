cc5a61242a8dea22add92834fec2ba19021a11ec
한세환 <abovenormal5023@gmail.com>
2022-07-21 14:43:50 +0900

    p149 IndexController 수정

--- src/main/java/com/david/book/springboot/web/IndexController.java
+++ src/main/java/com/david/book/springboot/web/IndexController.java
@@ -12,7 +12,9 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+
+        model.addAttribute("posts",postsService.findAllDesc());
         return "index";
     }
 
@@ -21,11 +23,6 @@ public class IndexController {
         return "posts-save";
     }
 
-    @GetMapping("/")
-    public String index(Model model){
 
-        model.addAttribute("posts",postsService.findAllDesc());
-        return "index";
-    }
 
 }
