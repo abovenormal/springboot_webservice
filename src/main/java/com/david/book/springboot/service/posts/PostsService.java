d046bf5b0f01baaaf9e6ec7e67ca74ceed86e77f
한세환 <abovenormal5023@gmail.com>
2022-07-21 14:19:10 +0900

    p149 PostListResponseDto 생성

--- src/main/java/com/david/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/david/book/springboot/service/posts/PostsService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.stream.Collectors;
+import java.util.List;
 
 
 @RequiredArgsConstructor
@@ -37,8 +38,8 @@ public class PostsService {
         return new PostsResponseDto(entity);
     }
 
-    @Transactional
-    public Lists<PostsListResponseDto> findAllDesc(){
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
         return postsRepository.findAllDesc().stream()
                 .map(PostsListResponseDto::new)
                 .collect(Collectors.toList());
